8dc358771b26a6bd423c7acc46b0054f34ce3ce9
sa2203 <samrudhi2203@gmail.com>
2025-06-11 19:03:47 +0530

    Final code of Todo list

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,15 +1,54 @@
 let b1 = document.getElementById("b1");
-let input = document.getElementById('input');
-let todolist = document.querySelector(".list-elem")
+let input = document.getElementById("input");
+let todolist = document.querySelector(".list-elem");
 
-const addToTodo = (e)=>{
+const getTodo = () => {
+    return JSON.parse(localStorage.getItem("todoList")) || [];
+};
+
+const addLocalStorage = (localList) => {
+    localStorage.setItem("todoList", JSON.stringify(localList));
+};
+
+let locallist = getTodo();
+
+const addToTodo = (e) => {
     e.preventDefault();
-    const divElem = document.createElement("div");
-    divElem.classList.add("todolist");
-    divElem.innerHTML = `<li></li>
-                <button id="b2">Delete</button>`;
-}
+    const inputValue = input.value.trim();
+    if (inputValue !== "" && !locallist.includes(inputValue)) {
+        locallist.push(inputValue);
+        addLocalStorage(locallist);
+        createListItem(inputValue);
+    }
+    input.value = "";
+};
 
-b1.addEventListener('click', (e)=>{
-    addToTodo(e);
-})
+const createListItem = (text) => {
+    const li = document.createElement("li");
+    li.textContent = text;
+
+    const b2 = document.createElement("button");
+    b2.textContent = "Delete";
+    b2.classList.add("b2");
+
+    b2.addEventListener("click", () => {
+        li.remove();
+        locallist = locallist.filter((todo) => todo !== text);
+        addLocalStorage(locallist);
+    });
+
+    li.appendChild(b2);
+    todolist.appendChild(li);
+};
+
+const showTodo = () => {
+    locallist.forEach((currEle) => {
+        createListItem(currEle);
+    });
+};
+
+// Initial display
+showTodo();
+
+// Add new todo
+b1.addEventListener("click", addToTodo);
